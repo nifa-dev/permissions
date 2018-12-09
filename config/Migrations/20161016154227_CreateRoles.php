9600ee5f353ffac4f4f828c71b53ca768ae35098
nifa-dev <jared.testa@nifa.aero>
2018-12-09 17:14:28 +1100

    roles-migration
    added default for Created and Modified column as NULL

--- config/Migrations/20161016154227_CreateRoles.php
+++ config/Migrations/20161016154227_CreateRoles.php
@@ -25,11 +25,11 @@ class CreateRoles extends AbstractMigration
         ]);
         $table->addColumn('created', 'datetime', [
             'default' => null,
-            'null' => false,
+            'null' => true,
         ]);
         $table->addColumn('modified', 'datetime', [
             'default' => null,
-            'null' => false,
+            'null' => true,
         ]);
         $table->create();
     }
